58ca64671779a8a10e3399188acbc660c58cc115
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-12-29 15:01:09 +0900

    springbootを追加

--- ChannelChecker/build.gradle.kts
+++ ChannelChecker/build.gradle.kts
@@ -1,5 +1,11 @@
 plugins {
     kotlin("jvm")
+    kotlin("plugin.spring") version "1.8.0"
+    id("org.springframework.boot") version "3.0.1"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("io.github.lognet.grpc-spring-boot") version "4.9.1"
+
+    application
 }
 
 group = "com.github.raink1208.channelchecksystem"
@@ -12,9 +18,24 @@ repositories {
 dependencies {
     implementation(project(":common"))
     implementation(kotlin("stdlib"))
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("io.github.lognet:grpc-spring-boot-starter:4.9.1")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
