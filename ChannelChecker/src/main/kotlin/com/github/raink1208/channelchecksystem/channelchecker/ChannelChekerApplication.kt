edd1612317798223f57511f4cb3abea208a680ff
raink1208 <58714901+raink1208@users.noreply.github.com>
2022-12-29 15:01:29 +0900

    SpringのApplicationを追加

---
+++ ChannelChecker/src/main/kotlin/com/github/raink1208/channelchecksystem/channelchecker/ChannelChekerApplication.kt
@@ -0,0 +1,11 @@
+package com.github.raink1208.channelchecksystem.channelchecker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChannelCheckerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChannelCheckerApplication>(*args)
+}
